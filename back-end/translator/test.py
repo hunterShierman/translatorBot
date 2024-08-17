9da2f37777b6e2cf54ac684fe1d31f6b74640f6e
Hunter Shierman <hshierman@gmail.com>
2024-08-17 01:18:34 -0400

    finished translator function

---
+++ back-end/translator/test.py
@@ -0,0 +1,5 @@
+text1 = "Bonjour, je suis le Dr. Paul Martin, professeur en biologie. Je suis enthousiaste à l'idée de collaborer avec vous."
+text2 = "Hello, I am Claire Dubois, Software Engineer at ABC Tech. I hope we can work together effectively."
+
+info1 = [text1, "en", "fr"]
+info2 = [text2, "fr", "en"]
