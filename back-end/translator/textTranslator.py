6a763ef1fcff7ebd0db6e2a6baa6a00a53d4fcda
Hunter Shierman <hshierman@gmail.com>
2024-08-17 01:19:47 -0400

    translator function

--- back-end/translator/textTranslator.py
+++ back-end/translator/textTranslator.py
@@ -4,24 +4,11 @@ from test import info1, info2
 
 translator = Translator()
 
-# language = input("what language? (en/fr): ")
-# text = input("What do you need translated: ")
-
-#english to french
-# if language == "en":
-#     words = translator.translate(text, src="en", dest="fr")
-#     print(type(words))
-
-# print("French to english: ", translator.translate(info1[0], src= info1[1], dest= info1[2]).text)
-# print("English to french: ", translator.translate(info2[0], src= info2[1], dest= info2[2]).text)
-    
 
 # function to translate
 def myTranslator(text, sourceLanguage, targetLanguage):
     translatedText = translator.translate(text, src = sourceLanguage, dest = targetLanguage).text
-    print(translatedText)
 
-myTranslator("Hello, I am Claire Dubois, Software Engineer at ABC Tech. I hope we can work together effectively.", "en", "fr")
-
+    return translatedText
 
 
