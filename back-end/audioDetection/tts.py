a56a91ede31b2086601796f33dcea50792ea340c
cjskora <cjskora@uwaterloo.ca>
2024-08-17 20:56:27 -0400

    chenged-the-names of files

--- back-end/audioDetection/tts.py
+++ back-end/audioDetection/tts.py
@@ -4,10 +4,11 @@ import pyttsx3
 from playsound import playsound
 import tempfile
 
-text = "Welcome to our chatbot and language learner."
-filename = 'back-end/audioDetection/output.txt'
-def tts(text):
-    tts = gTTS(text)
+text_english = "Welcome to our chatbot and language learner."
+text_french = "Bienvenue sur notre chatbot et apprenant de langues."
+
+def tts(text, lang='en'):
+    tts = gTTS(text, lang=lang)
     temp_file = tempfile.NamedTemporaryFile(suffix=".mp3", delete=False)
 
     tts.save(temp_file.name)
@@ -15,6 +16,12 @@ def tts(text):
     temp_file.close()
     return temp_file.name
 
-file_path = tts(text)
-playsound(file_path)
-os.remove(file_path)
+# Play English text
+file_path_english = tts(text_english, lang='en')
+playsound(file_path_english)
+os.remove(file_path_english)
+
+# Play French text
+file_path_french = tts(text_french, lang='fr')
+playsound(file_path_french)
+os.remove(file_path_french)
